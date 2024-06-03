ddaa08a1ddda2c9e5266d3c4cbe55b72c580ecc9
Yuriy Kaminskiy <yumkam7@ydb.tech>
2024-06-03 23:03:37 +0300

    force-disable wide_top_sort spilling (buggy)

--- ydb/library/yql/minikql/comp_nodes/mkql_wide_top_sort.cpp
+++ ydb/library/yql/minikql/comp_nodes/mkql_wide_top_sort.cpp
@@ -525,7 +525,11 @@ private:
     EOperatingMode GetMode() const { return Mode; }
 
     bool HasMemoryForProcessing() const {
+#if 0
         return !TlsAllocState->IsMemoryYellowZoneEnabled();
+#else
+        return true;
+#endif
     }
 
     bool IsReadFromChannelFinished() const {
