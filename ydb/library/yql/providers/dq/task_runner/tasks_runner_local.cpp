yumkam/ydb

commit 59edb07f48920f549bf86b0fe5317a7b144209fd

Added log function to dqTaskRunner (#5772)

ydb/library/yql/providers/dq/task_runner/tasks_runner_local.cpp
@@ -262,7 +262,10 @@ public:
         }
         auto ctx = ExecutionContext;
         ctx.FuncProvider = TaskTransformFactory(settings.TaskParams, ctx.FuncRegistry);
-        return MakeDqTaskRunner(alloc, ctx, settings, { });
+        return MakeDqTaskRunner(alloc, ctx, settings, [taskId = task.GetId(), traceId = traceId](const TString& message) {
+            YQL_LOG_CTX_ROOT_SESSION_SCOPE(traceId, ToString(taskId));
+            YQL_CLOG(DEBUG, ProviderDq) << message;
+        });
     }
 
 private:

ydb/library/yql/dq/runtime/dq_output_channel.cpp
@@ -89,10 +89,6 @@ public:
     }
 
     void DoPush(NUdf::TUnboxedValue* values, ui32 width) {
-        ui64 rowsInMemory = PackedRowCount + ChunkRowCount;
-
-        LOG("Push request, rows in memory: " << rowsInMemory << ", bytesInMemory: " << (PackedDataSize + Packer.PackedSizeEstimate())
-            << ", finished: " << Finished);
         YQL_ENSURE(!IsFull());
 
         if (Finished) {
@@ -178,8 +174,6 @@ public:
 
     [[nodiscard]]
     bool Pop(TDqSerializedBatch& data) override {
-        LOG("Pop request, rows in memory: " << GetValuesCount() << ", finished: " << Finished);
-
         if (!HasData()) {
             PushStats.TryPause();
             if (Finished) {

commit 059e540a8805038502b619fa7134d3fe6488fee1

task_runner_local: avoid formatting log messages when loglevel is disabled
Callee depends on `!!logger` to avoid formatting messages that would be
thrown out. Setting logger unconditionally breaks this logic.

Regression by 59edb07f48920f549bf86b0fe5317a7b144209fd

TODO: turn `TLogFunc` into `std::function<void,std::function<TString>>`
and use lazy evaluation.

ydb/library/yql/providers/dq/task_runner/tasks_runner_local.cpp
@@ -261,11 +261,15 @@ public:
             settings.ReadRanges.push_back(readRange);
         }
         auto ctx = ExecutionContext;
+        TLogFunc logger {};
+        if (YQL_CLOG_ACTIVE(DEBUG, ProviderDq)) {
+            logger = [taskId = task.GetId(), traceId = traceId](const TString& message) {
+                YQL_LOG_CTX_ROOT_SESSION_SCOPE(traceId, ToString(taskId));
+                YQL_CLOG(DEBUG, ProviderDq) << message;
+            };
+        }
         ctx.FuncProvider = TaskTransformFactory(settings.TaskParams, ctx.FuncRegistry);
-        return MakeDqTaskRunner(alloc, ctx, settings, [taskId = task.GetId(), traceId = traceId](const TString& message) {
-            YQL_LOG_CTX_ROOT_SESSION_SCOPE(traceId, ToString(taskId));
-            YQL_CLOG(DEBUG, ProviderDq) << message;
-        });
+        return MakeDqTaskRunner(alloc, ctx, settings, logger);
     }
 
 private: