ce2ccaa32d53ce3dcd767e48c3424fb0e29e9341
Yuriy Kaminskiy <yumkam7@ydb.tech>
2024-09-23 23:40:26 +0000

    tpc: add decimal downloaders

--- ydb/library/benchmarks/runner/download_files_ds_1.sh
+++ ydb/library/benchmarks/runner/download_files_ds_1.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -eux
-if [ ! -d tpc/ds/1 ]; then
-mkdir -p tpc/ds/1
+if [ ! -d tpc/ds/1${decimal+_${decimal:-decimal}} ]; then
+mkdir -p tpc/ds/1${decimal+_${decimal:-decimal}}
 fi
 
 b=`pwd`
-cd tpc/ds/1
+cd tpc/ds/1${decimal+_${decimal:-decimal}}
 
-base=https://storage.yandexcloud.net/tpc/ds/s1/parquet
+base=https://storage.yandexcloud.net/tpc/ds/s1${decimal+_${decimal:-decimal}}/parquet
 
 source $b/download_lib.sh
 source $b/download_tpcds_tables.sh
