bbf2fecae8aea4b3a687ec360b7dc37e85206777
Yuriy Kaminskiy <yumkam7@ydb.tech>
2024-11-17 00:04:09 +0300

    more debug prints

--- ydb/library/yql/providers/pq/async_io/dq_pq_write_actor.cpp
+++ ydb/library/yql/providers/pq/async_io/dq_pq_write_actor.cpp
@@ -210,6 +210,7 @@ public:
             Buffer.push(std::move(data));
             return true;
         })) {
+            Cerr << TInstant::Now() << ' ' << LogPrefix << " PQ SendData Early return @ " << NextSeqNo << Endl;
             return;
         }
 
@@ -233,6 +234,7 @@ public:
         while (HandleNewPQEvents()) { } // Write messages while new continuationTokens are arriving
 
         if (FreeSpace <= 0) {
+            Cerr << TInstant::Now() << ' ' << LogPrefix << " Suspend " << NextSeqNo << ' ' << FreeSpace << Endl;
             ShouldNotifyNewFreeSpace = true;
         }
     };
@@ -356,6 +358,7 @@ private:
             }
 
             if (ShouldNotifyNewFreeSpace && FreeSpace > 0) {
+                Cerr << TInstant::Now() << ' ' << LogPrefix << " Resume " << NextSeqNo << ' ' << FreeSpace << Endl;
                 Callbacks->ResumeExecution();
                 ShouldNotifyNewFreeSpace = false;
             }
