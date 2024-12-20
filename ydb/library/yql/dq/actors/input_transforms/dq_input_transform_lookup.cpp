66221a6ba24c498d4f63eb13df0566b2412456e3
Yuriy Kaminskiy <yumkam7@ydb.tech>
2024-12-20 11:29:28 +0300

    check a lot of unique sizes

--- ydb/library/yql/dq/actors/input_transforms/dq_input_transform_lookup.cpp
+++ ydb/library/yql/dq/actors/input_transforms/dq_input_transform_lookup.cpp
@@ -289,7 +289,7 @@ private: //IDqComputeActorAsyncInput
                 if (MaxKeys == MaxKeysInRequest)
                     MaxKeys = 13;
                 else
-                    ++(MaxKeys *= 2);
+                    MaxKeys += 5;//++(MaxKeys *= 2);
                 LruHits->Add(ReadyQueue.RowCount());
                 LruMiss->Add(AwaitingQueue.size());
             }
