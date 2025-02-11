04bd48dd86dcf0f389795a74c8b67150ed4d9e7e
Yuriy Kaminskiy <yumkam7@ydb.tech>
2025-02-11 20:03:37 +0300

    fix incorrect FlushRequested logic

--- ydb/public/sdk/cpp/src/client/persqueue_public/impl/write_session_impl.cpp
+++ ydb/public/sdk/cpp/src/client/persqueue_public/impl/write_session_impl.cpp
@@ -987,9 +987,10 @@ void TWriteSessionImpl::ResetForRetryImpl() {
 void TWriteSessionImpl::FlushWriteIfRequiredImpl() {
     Y_ABORT_UNLESS(Lock.IsLocked());
 
-    if (!CurrentBatch.Empty() && !CurrentBatch.FlushRequested) {
+    if (!CurrentBatch.Empty()) {
         MessagesAcquired += static_cast<ui64>(CurrentBatch.Acquire());
         if (TInstant::Now() - CurrentBatch.StartedAt >= Settings.BatchFlushInterval_.value_or(TDuration::Zero())
+            || CurrentBatch.FlushRequested
             || CurrentBatch.CurrentSize >= Settings.BatchFlushSizeBytes_.value_or(0)
             || CurrentBatch.CurrentSize >= MaxBlockSize
             || CurrentBatch.Messages.size() >= MaxBlockMessageCount
