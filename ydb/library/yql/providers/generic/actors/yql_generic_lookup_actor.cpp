8e032105f3592a1e8c16d01f9bea8f14660e1f37
Yuriy Kaminskiy <yumkam7@ydb.tech>
2026-02-19 22:36:52 +0300

    fixup! stray Cerr

--- ydb/library/yql/providers/generic/actors/yql_generic_lookup_actor.cpp
+++ ydb/library/yql/providers/generic/actors/yql_generic_lookup_actor.cpp
@@ -460,7 +460,7 @@ namespace NYql::NDq {
 #if 1 // Temporary workaround for not-yet-deployed YQ-5124
             Y_DEBUG_ABORT_UNLESS(state->FullscanLimit == 0 || state->FullscanLimit > state->ResultRows);
             if (state->FullscanLimit > 0 && height > state->FullscanLimit - state->ResultRows) {
-                YQL_CLOG(WARN, ProviderGeneric) << "ActorId=" << SelfId() <<<< " YQ-5124 Overshoot " << height << " > " << state->FullscanLimit << " - " << state->ResultRows;
+                YQL_CLOG(WARN, ProviderGeneric) << "ActorId=" << SelfId() << " YQ-5124 Workaround for unimplemented LIMIT invoked " << height << " > " << state->FullscanLimit << " - " << state->ResultRows;
                 height = state->FullscanLimit - state->ResultRows;
             }
 #endif
